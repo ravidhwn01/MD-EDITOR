78977f24c7fce2dc72b5e6e0a0be0451c56d006e
ravi kumar dhawan <ravidhawan8899@gmail.com>
2022-12-03 03:30:53 +0530

    markdown editor

--- markdownEditor/src/components/MarkdownReact.tsx
+++ markdownEditor/src/components/MarkdownReact.tsx
@@ -1,19 +1,28 @@
-import { useState } from "react"
-
-type Props = {}
+import { useState } from "react";
+import ReactMarkdown from "react-markdown";
+type Props = {};
 
 function MarkdownReact({}: Props) {
- const [markdown, setMarkdown] = useState("")
+  const [markdown, setMarkdown] = useState("");
   return (
     <>
-    <div className="center_div">
-        <textarea className="left_side"  id=""  cols={100}  rows = {20} value = {markdown} onChange = {(e)=>{
-            setMarkdown(e.target.value)
-        }}  ></textarea>
-        <div className="right_side">{markdown}</div>
-    </div>
+      <div className="center_div">
+        <textarea
+          className="left_side"
+          id=""
+          cols={100}
+          rows={20}
+          value={markdown}
+          onChange={(e) => {
+            setMarkdown(e.target.value);
+          }}
+        ></textarea>
+        <div className="right_side">
+          <ReactMarkdown>{markdown}</ReactMarkdown>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default MarkdownReact
+export default MarkdownReact;
